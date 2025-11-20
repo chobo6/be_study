dee500f0387d92ceb1bc6f15dbd58e1251097bed
chobo6 <hongjegue@naver.com>
2025-11-20 11:42:42 +0900

    JAVA 퀴즈 33 (퀴즈32 + ArrayList)

---
+++ src/be_study/quiz/quiz33/ChineseRestaurant.java
@@ -0,0 +1,52 @@
+package be_study.quiz.quiz33;
+
+import java.util.ArrayList;
+
+public class ChineseRestaurant {
+
+	public static void main(String[] args) {
+		// 주문을 위한 메뉴 선정
+		Menu jjajang = new Menu("짜장", 4900);
+		Menu jjambbong = new Menu("짬뽕", 5900);
+		Menu tangsook = new Menu("탕수육", 13900);
+		// 메뉴를 조합하여 주문을 생성
+//		Menu[] menuArr = { jjajang, jjambbong, tangsook };
+		ArrayList<Menu> menuArr = new ArrayList<>();
+		menuArr.add(jjajang);
+		menuArr.add(jjambbong);
+		menuArr.add(tangsook);
+		Order order = new Order(123, menuArr);
+		// 주문 결과 출력
+		System.out.printf("주문 합계: %d원\n", order.totalPrice());
+		}
+}
+class Order {
+	// 필드(인스턴스 필드)
+	int orderNum;
+	ArrayList<Menu> menus;
+	// 생성자
+	public Order(int i, ArrayList<Menu> menuArr) {
+		orderNum = i;
+		menus = menuArr;
+	}
+	// 메소드(인스턴스 메소드)
+	public int totalPrice() {
+	/* 1. 모든 주문 메뉴의 총합을 반환하세요. */
+		int sum = 0;
+		for(Menu i : menus) {
+			sum += i.price; 
+		}
+		return sum;
+	}
+}
+class Menu {
+	// 필드
+	String name;
+	int price;
+	// 생성자
+	public Menu(String str, int i) {
+		name = str;
+		price = i;
+	}
+
+}
