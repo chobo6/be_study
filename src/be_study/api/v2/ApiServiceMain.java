f0285eff0db9eb50e2c6163258355d4821e114e3
chobo6 <hongjegue@naver.com>
2026-01-16 09:18:22 +0900

    공공데이터 API 구조개선(v1, v2), 파라미터 활용

---
+++ src/be_study/api/v2/ApiServiceMain.java
@@ -0,0 +1,29 @@
+package be_study.api.v2;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class ApiServiceMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		MidFcstInfoServiceRepository repository = new MidFcstInfoServiceRepository();
+		
+		MidTaDTO midTaDTO = repository.getMidTa("202601150600", "11B10101");
+		// 202601150600 시간 기준, 11B10101 서울 날씨
+		
+		// midTaDTO 활용
+		System.out.println(midTaDTO);
+		
+		MidTaDTO midTaDTO2 = repository.getMidTa("202601150600", "11B20101");
+		// 202601150600 시간 기준, 11B20101 인천 날씨
+		
+		// midTaDTO 활용
+		System.out.println(midTaDTO2);
+		
+		// 파싱한 DTO 객체 -> DB 저장
+	}
+
+}
